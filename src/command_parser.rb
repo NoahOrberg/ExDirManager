88c5f3050cca04e10620d6b5f3146c65bbe7b1c2
NoahOrberg <noah.orberg.9999@gmail.com>
2017-07-17 21:52:23 +0900

    modify default value

--- src/command_parser.rb
+++ src/command_parser.rb
@@ -8,8 +8,11 @@ class CommandParser
 
     def parse
         mode = @argv[0]
-        dir_name = @argv[1]
+        dir_name = @argv[1].nil? ? "Prog0":@argv[1]
         hash_argv = Hash[*@argv[2..-1]]
-        Command.new(mode, dir_name, hash_argv["-h"], hash_argv["-m"], hash_argv["-f"])
+        header = hash_argv["-h"].nil? ? "Ex":hash_argv["-h"]
+        max = hash_argv["-m"].nil? ? "10":hash_argv["-m"]
+        digit = hash_argv["-f"].nil? ? "2":hash_argv["-f"]
+        Command.new(mode, dir_name, header, max, digit)
     end
 end
